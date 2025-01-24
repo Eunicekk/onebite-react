17cc093fda9cd1de31aa74c7f52e195a1e3f4a8e
Eunicekk <kate4234@gmail.com>
2025-01-24 17:37:59 +0900

    [feat] day10 과제 업로드

---
+++ homework/day10/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name={"김윤"} isMember={true} />
+    </>
+  );
+}
+
+export default App;
