9b1be4257322924cec5f2694f1346d4f80fba1e8
Eunicekk <kate4234@gmail.com>
2025-01-31 19:06:24 +0900

    [feat] section03 업로드

---
+++ section03/src/math.js
@@ -0,0 +1,16 @@
+// math 모듈
+
+// 덧셈 기능
+export function add(a, b) {
+  return a + b;
+}
+
+// 뺄셈 기능
+export function sub(a, b) {
+  return a - b;
+}
+
+// 곱셈 기능
+export default function multiply(a, b) {
+  return a * b;
+}
